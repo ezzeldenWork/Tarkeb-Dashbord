ce7588a324ed9773a7a0dc309bd473a97f489222
ezzelden.work <ezzelden.work@gmail.com>
2023-05-02 15:23:13 +0300

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
       "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/English.json";
   }
 
-  $(".chat-content").scrollTop($(".end-div").offset().top);
+
   //  side menu
 
   $(document).click(function (e) {
@@ -66,4 +66,22 @@ $(document).ready(function () {
     },
     scrollX: true,
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  $(".chat-content").scrollTop($(".end-div").offset().top);
 });
